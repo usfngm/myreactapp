1149abbc54e2ae93b8a9b89c5ea7d784d0cde882
Youssef Negm <youssefnegm@Youssefs-MacBook-Air.local>
2019-10-13 03:40:09 +0200

    test

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Welcome to my calculator app</h1>
+      <h1>Welcome to my useless app</h1>
       <Container>
         <Form>
           <Row>
